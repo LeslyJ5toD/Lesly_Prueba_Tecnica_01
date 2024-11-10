27e65c639497085a300b1d5205a23cea276a2e36
Lesly Rosina Jiménez Sánchez <leslyj242526@gmail.com>
2024-11-10 00:56:50 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesly_Prueba_Tecnica_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teatro teatro = new Teatro();
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("-------------------- Sistema de Reserva de Asientos --------------------\n");
+                Console.WriteLine("1. Ver mapa de asientos");
+                Console.WriteLine("2. Reservar un asiento");
+                Console.WriteLine("3. Salir");
+                Console.Write("\nSeleccione una opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        teatro.MostrarMapa();
+                        break;
+                    case 2:
+                        teatro.ReservarAsiento();
+                        break;
+                    case 3:
+                        Console.WriteLine("Gracias por usar el sistema. ¡Hasta luego!");
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida. Intente nuevamente.");
+                        break;
+                }
+
+            } while (opcion != 3);
+        }
+    }
+}
+    
+
---
+++ Teatro.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesly_Prueba_Tecnica_01
+{
+    internal class Teatro
+    {
+        
+        private const int Filas = 10;
+        private const int AsientosPorFila = 10;
+        private char[,] asientos;
+
+        public Teatro()
+        {
+            asientos = new char[Filas, AsientosPorFila];
+            for (int i = 0; i < Filas; i++)
+            {
+                for (int j = 0; j < AsientosPorFila; j++)
+                {
+                    asientos[i, j] = 'L'; 
+                }
+            }
+        }
+
+        public void MostrarMapa()
+        {
+            Console.Clear();
+            Console.WriteLine("---- Mapa de Asientos ----\n");
+
+            for (int i = 0; i < Filas; i++)
+            {
+                Console.Write("Fila " + (i + 1) + ": ");
+                for (int j = 0; j < AsientosPorFila; j++)
+                {
+                    Console.Write(asientos[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ReservarAsiento()
+        {
+            Console.Write("\nIngrese el número de fila (1-10): ");
+            int fila = int.Parse(Console.ReadLine()) - 1;
+
+            Console.Write("Ingrese el número de asiento (1-10): ");
+            int asiento = int.Parse(Console.ReadLine()) - 1;
+
+            if (fila >= 0 && fila < Filas && asiento >= 0 && asiento < AsientosPorFila)
+            {
+                if (asientos[fila, asiento] == 'L')
+                {
+                    asientos[fila, asiento] = 'X';
+                    Console.WriteLine("¡Reserva exitosa! Asiento marcado como ocupado.");
+                }
+                else
+                {
+                    Console.WriteLine("El asiento ya está reservado. Elija otro asiento.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El número de fila o asiento está fuera de rango.No somos enfermeria para hacer reventa");
+            }
+        }
+    }
+}
+    
+
